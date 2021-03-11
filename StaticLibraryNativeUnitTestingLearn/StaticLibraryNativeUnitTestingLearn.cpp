3613cc6703763729c3aab45213bc638a4d6e67a9
alvandi <geradylanalvandi@gmail.com>
2021-03-11 21:31:04 +0100

    Add project files.

---
+++ StaticLibraryNativeUnitTestingLearn/StaticLibraryNativeUnitTestingLearn.cpp
@@ -0,0 +1,17 @@
+// StaticLibraryNativeUnitTestingLearn.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+int Add(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+/* Subtract num 2 from num 1*/
+int Subtract(int num1, int num2)
+{
+	return num1 - num2;
+}
